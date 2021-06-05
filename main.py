15c97f4a3f3cb71520573ae7d39d88767ce72946
Ashok Arora <arora8715@gmail.com>
2021-06-05 19:10:55 +0400

    List file names

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 from tkinter import Scrollbar
-import PySimpleGUI as sg
-from PySimpleGUI.PySimpleGUI import Input
 
+import PySimpleGUI as sg
+# from PySimpleGUI.PySimpleGUI import Input
+
+import os
+import time
 
 def assign_layout():
     tabs = [
         [
             sg.Tab("Files", [
                 [
-                    # TODO: Path to file/folder
-                    sg.Input("Path to file/folder", size=(80, 1), key='-INPUT-', readonly=True), sg.FilesBrowse("File(s)"), sg.FolderBrowse("Folder")
+                    # sg.Input("Path to file/folder", size=(80, 1), key='-INPUT-', readonly=True), 
+                    sg.Input(key='-FILE-', enable_events=True, visible=False), sg.FilesBrowse("File(s)", enable_events=True, target='-FILE-'),
+                    sg.Input(key='-FOLDER-', enable_events=True, visible=False), sg.FolderBrowse("Folder", enable_events=True, target='-FOLDER-')
 
                 ],
                 [
                     # TODO: List of files selected or files inside folder
-                    sg.Text(""),
+                    sg.Listbox([], size=(80, 25), right_click_menu=[[]], key='-LIST-')
+                    # sg.Multiline("", key='-List-'),
 
                     # TODO: Button for each entry to RUN the file (attach to the container)
-                    sg.Button("")
+                    # sg.Button("")
 
                     # for each_file in list_of_files
                 ],
                 [
                     # TODO: RUN AND CSV buttons
-                    sg.Button("RUN"), sg.Button("Save to CSV")
+                    sg.Button("RUN", key='-RUN-')
                 ]
 
-            ]),
+            ], element_justification='c'),
             sg.Tab("Input", [
                 [
                     sg.Multiline(size=(800, 600))
@@ -38,6 +43,17 @@ def assign_layout():
 
                 ]
             ]),
+            sg.Tab("Results", [
+                [
+                    sg.Text(" ")
+                ],
+                [
+                    sg.Listbox([], size=(80, 25), right_click_menu=[[]], key='-RESULT-')
+                ],
+                [
+                    sg.Button("Save to CSV", key='-SAVE-')
+                ]
+            ], element_justification='c')
         ]
     ]
 
@@ -48,7 +64,7 @@ def assign_layout():
         "Remote Code Executor", layout, size=(800, 600), element_justification="c", resizable=True, finalize=True
     )
     # window['-INPUT-'].Widget.configure(highlightcolor='red', highlightthickness=2)
-    window['-INPUT-'].Widget.configure(highlightcolor='grey', highlightthickness=2)
+    # window['-INPUT-'].Widget.configure(highlightcolor='grey', highlightthickness=2)
 
     return window
 
@@ -74,10 +90,43 @@ def main():
 
     sg.set_options(font=("Montserrat", 10))
     window = assign_layout()
+
+    l = []
+
     while True:
         event, values = window.read()
+        # print(event, values)
         if event in (None, sg.WINDOW_CLOSED):
             break
+
+        if event in '-FILE-':
+            l = list(map(os.path.basename, values['-FILE-'].split(';')))
+            window['-LIST-'].update(l)
+            window.refresh()
+
+        if event in '-FOLDER-':
+            l = os.listdir(values['-FOLDER-'])
+            window['-LIST-'].update(l)
+            window.refresh()
+        
+        if event in '-RUN-':
+            length = len(l)
+            if length == 0:
+                sg.popup("No files selected, aborting run.")
+            else:
+                layout = [[sg.ProgressBar(1, orientation='h', size=(20, 20), key='progress')]]
+                progress_window = sg.Window('Running in progress', layout).Finalize()
+                progress_bar = progress_window.FindElement('progress')
+
+                for i in range(0, length+1):
+                    progress_bar.UpdateBar(i, length)
+                    time.sleep(.5)
+                time.sleep(.3)
+                progress_window.close()
+                sg.popup("Run completed, check 'Results' tab")
+                # window.finalize()
+                # window['-RESULT-'].update(visible=True)
+                # window.refresh()
 
 
 if __name__ == "__main__":
